d72bb0d23eeba65b627f42a079d10862004a2fe3
AnuragRaj11 <Araj59519@gmail.com>
2024-06-24 13:06:24 +0530

    e

--- Backend/main.js
+++ Backend/main.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const blog = require('./routes/blog')
 const app = express()
 const port = 3000
 
+
 app.use(express.static('public'))
+ app.use('/blog', blog)
 
 app.get('/', (req, res) => {
     res.send('hello express')
 })
 
-app.get('/blog', (req, res) => {
+app.get('/about', (req, res) => {
     res.send('hello blog')
 })
 
