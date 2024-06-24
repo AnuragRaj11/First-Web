d58e12aaa14e3ea4471bc9b7a784a346aa806331
AnuragRaj11 <Araj59519@gmail.com>
2024-06-24 13:20:02 +0530

    MERN Learning

--- Backend/main.js
+++ Backend/main.js
@@ -3,16 +3,40 @@ const blog = require('./routes/blog')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
- app.use('/blog', blog)
+//app.use(express.static('public'))
+app.use('/blog', blog)
+
+
+app.use((req, res, next) => {
+    //console.log(req.headers)
+    req.A = "I am AR bhai";
+    console.log(`${Date.now()} is a ${req.method}`)
+    // res.send("Hacked by Middlware 1")
+    next()
+})
+
+// Middleware 2
+app.use((req, res, next) => {
+    console.log('m2')
+    req.harry = "I am cool bro";
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('hello express')
 })
 
+// app.get('/', (req, res) => {
+//     let siteName = "Adidas"
+//     let searchText = "Search Now"
+//     let arr = ["Hey", 54, 65]
+//     res.render("index", { siteName: siteName, searchText: searchText, arr })
+// })
+
 app.get('/about', (req, res) => {
-    res.send('hello blog')
+    res.send('hello mid!'+req.A)
 })
 
 app.listen(port, () => {
--- Backend/routes/blog.js
+++ Backend/routes/blog.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+router.use((req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+  })
+
 
 router.get('/', (req, res) => {
     res.send('Blog home page')
